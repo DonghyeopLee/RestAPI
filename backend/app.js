e51bf2db352c11c0d9a9b612cc077bf548dda6ad
Ian Lee <87459518+DonghyeopLee@users.noreply.github.com>
2024-09-06 21:20:46 +1000

    update

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import {MongoAPI} from "./cred.js"
 
 const app = express();
+mongoose.connect(MongoAPI
+).then(() => app.listen(5000))
+.then(() => 
+    console.log("Server is running on port 5000")
+)
+.catch((error) => console.log(error));
 
-app.use("/api", (req, res, next) => {
-    res.send("hello World!!");
-});
 
-app.listen(5000);
 
 
 
